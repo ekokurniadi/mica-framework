ba2083ab1654cb30f032016f05a040bf3334baf7
Ekokurniadi <ekokurniadi.02@gmail.com>
2022-01-03 15:58:11 +0700

    load from env file

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -11,17 +11,25 @@ import (
 )
 
 func main() {
-	dsn := "root:@tcp(127.0.0.1:3306)/database?charset=utf8mb4&parseTime=True&loc=Local"
+
+	env := godotenv.Load()
+	if env != nil {
+		log.Fatal(env.Error())
+	}
+
+	user := os.Getenv("USER_HOST")
+	password := os.Getenv("USER_PASS")
+	host := os.Getenv("HOST")
+	database := os.Getenv("DATABASE_NAME")
+	databasePort := os.Getenv("DATABASE_PORT")
+
+	dsn := "" + user + ":" + password + "@tcp(" + host + ":" + databasePort + ")/" + database + "?charset=utf8mb4&parseTime=True&loc=Local"
 	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	s := godotenv.Load()
-	if s != nil {
-		fmt.Println(s)
-	}
 	router := gin.Default()
 	router.Run()
 }
