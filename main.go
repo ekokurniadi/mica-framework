d717adca7ad573bc50ae41d83c07e3c19c5c3216
Ekokurniadi <ekokurniadi.02@gmail.com>
2022-01-03 15:47:36 +0700

    up

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/database?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	s := godotenv.Load()
+	if s != nil {
+		fmt.Println(s)
+	}
+	router := gin.Default()
+	router.Run()
 }
